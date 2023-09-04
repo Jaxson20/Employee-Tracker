d2304e0dfd08155b22885f2de0b093add816c898
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-09-04 11:41:49 -0500

    new files

---
+++ assets/schema-files/Employees-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  job_title VARCHAR(255) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT, -- Foreign key reference to departments table
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
